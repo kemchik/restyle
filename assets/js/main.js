a0ea5c803586ce7611e86c351a8ce99df3fccb45
kemchik <kerminsk@gmail.com>
2017-01-03 13:38:49 +0300

    add js slide show

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,31 @@
 				$body.addClass('is-loading');
 
 			$window.on('load', function() {
-				window.setTimeout(function() {
+				window.setTimeout(function() 
+
+				{
 					$body.removeClass('is-loading');
 				}, 100);
 			});
+
+			function slideSwitch() {
+			    var $active = $('#slideshow IMG.active');
+			    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+			    var $sibs  = $active.siblings();
+			    var rndNum = Math.floor(Math.random() * $sibs.length );
+			    var $next  = $( $sibs[ rndNum ] );
+			    $active.addClass('last-active');
+
+			    $next.css({opacity: 0.0})
+			       .addClass('active')
+			       .animate({opacity: 1.0}, 1000, function() {
+			                 $active.removeClass('active last-active');
+			       });
+			    }
+
+			   $(function() {
+			       setInterval( "slideSwitch()", 5000 );
+			   });
 
 		// Forms.
 
